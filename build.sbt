333f1415718fa114cb02f751f96242b9d532a0cc
Kiran Kumar <kirankumar@RLYLTPZ14.local>
2020-10-14 22:50:47 +0200

    [15] Add Flyway

--- build.sbt
+++ build.sbt
@@ -5,10 +5,16 @@ scalaVersion := "2.13.2"
 //AKkA Stack
 val AkkaVersion = "2.6.8"
 val AkkaHttpVersion = "10.2.1"
+val SlickVersion = "3.3.3"
+
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor-typed" % AkkaVersion,
   "com.typesafe.akka" %% "akka-http" % AkkaHttpVersion,
   "com.typesafe.akka" %% "akka-stream" % AkkaVersion,
-  "com.typesafe.akka" %% "akka-http-spray-json" % AkkaHttpVersion
+  "com.typesafe.akka" %% "akka-http-spray-json" % AkkaHttpVersion,
+  "com.typesafe.slick" %% "slick" % SlickVersion,
+  "com.typesafe.slick" %% "slick-hikaricp" % SlickVersion,
+  "org.flywaydb" % "flyway-core" % "7.0.2",
+  "org.postgresql" % "postgresql" % "9.4-1206-jdbc42"
 )
 
--- src/main/scala/mqtt/simulator/Main.scala
+++ src/main/scala/mqtt/simulator/Main.scala
@@ -3,7 +3,9 @@ package mqtt.simulator
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.Http
+import com.typesafe.config.ConfigFactory
 import mqtt.simulator.api.API
+import slick.jdbc.JdbcBackend.Database
 
 import scala.io.StdIn
 
@@ -11,6 +13,14 @@ object Main extends App {
 
   implicit val system = ActorSystem(Behaviors.empty, "mqtt-simulator")
   implicit val executionContext = system.executionContext
+
+  //Config
+  val config = ConfigFactory.load()
+
+  //FlyWay
+  val db = Database.forConfig("db")
+  FlywayMigration(config, "db")
+
 
   // HTTP API to interact with Simulation
   val bindAndFuture = Http().newServerAt("localhost", 8080).bind(API.route)
